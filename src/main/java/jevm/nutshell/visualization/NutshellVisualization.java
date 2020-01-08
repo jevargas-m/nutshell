f6c8bc35564fae6952c44e7f5368a7fc92fa1dd7
jevm-mcit <jevargas@seas.upenn.edu>
2020-01-08 15:06:02 -0600

    visualization allow many clouds on single file

--- src/main/java/jevm/nutshell/visualization/NutshellVisualization.java
+++ src/main/java/jevm/nutshell/visualization/NutshellVisualization.java
@@ -4,29 +4,30 @@ import jevm.nutshell.engine.ScoredWord;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class NutshellVisualization {
 
-    public int MAX_VALUE = 100;
-    public int MIN_VALUE = 10;
-    public String OUTPUT_FILENAME = "gword.js";
+    public static final int DEFAULT_MAX_SIZE = 100;
+    public static final int DEFAULT_MIN_SIZE = 20;
 
-    private List<ScoredWord> data;
-    private Map<String, Integer> normalizedData;
+    private int maxSize = DEFAULT_MAX_SIZE;
+    private int minSize = DEFAULT_MIN_SIZE;
 
-    public NutshellVisualization(List<ScoredWord> data) {
-        this.data = data;
-        normalize();
+    private Map<String, Map<String, Integer>> normalizedData;
+
+    public NutshellVisualization() {
+        normalizedData = new HashMap<>();
     }
 
-    private void normalize() {
+    public void addDataSet(String name, List<ScoredWord> data) {
+        normalizedData.put(name, normalize(data));
+    }
+
+    private Map<String, Integer> normalize(List<ScoredWord> data) {
+        Map<String, Integer> output = new HashMap<>();
+
         double min = Double.POSITIVE_INFINITY;
         double max = Double.NEGATIVE_INFINITY;
 
@@ -35,35 +36,90 @@ public class NutshellVisualization {
             if (sw.score < min) min = sw.score;
         }
 
-        double m = (MAX_VALUE - MIN_VALUE) / (max - min);
-        double b = MAX_VALUE - m * max;
+        double m = (maxSize - minSize) / (max - min);
+        double b = maxSize - m * max;
 
-        normalizedData = new HashMap<>();
         for (ScoredWord sw : data) {
             int score = (int) ((m * sw.score) + b);
-            normalizedData.put(sw.word, score );
+            output.put(sw.word, score );
         }
+        return output;
     }
 
-
-    public void createJSONFile() throws FileNotFoundException {
-        File f = new File(OUTPUT_FILENAME);
-        PrintWriter pw = new PrintWriter(f);
+    private String createJSONString(Map<String, Integer> dataMap) {
 
         JSONArray list = new JSONArray();
 
-        for (String word : normalizedData.keySet()) {
+        for (String word : dataMap.keySet()) {
             JSONObject obj = new JSONObject();
-            Integer score = normalizedData.get(word);
+            Integer score = dataMap.get(word);
             obj.put("text", word);
             obj.put("size", score);
             list.add(obj);
         }
 
-        pw.print("gword = ");
-        pw.print(list.toJSONString());
-        pw.close();
+        return list.toJSONString();
     }
 
 
+
+
+    public void createWordCloud(String filename) throws FileNotFoundException {
+        File f = new File(filename);
+        PrintWriter pw = new PrintWriter(f);
+
+        String html = "<!DOCTYPE html>\n" +
+                "<meta charset=\"utf-8\">\n" +
+                "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js\"></script>\n" +
+                "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/d3-cloud/1.2.5/d3.layout.cloud.js\"></script>\n" +
+                "<body>\n" +
+                "<script>\n" +
+                "    var fill = d3.scale.category20();\n" +
+                "    function draw(words) {\n" +
+                "        d3.select(\"body\").append(\"svg\")\n" +
+                "            .attr(\"width\", 1024)\n" +
+                "            .attr(\"height\", 800)\n" +
+                "            .append(\"g\")\n" +
+                "            .attr(\"transform\", \"translate(514, 400)\")\n" +
+                "            .selectAll(\"text\")\n" +
+                "            .data(words)\n" +
+                "            .enter().append(\"text\")\n" +
+                "            .style(\"font-size\", function(d) { return d.size + \"px\"; })\n" +
+                "            .style(\"font-family\", \"Impact\")\n" +
+                "            .style(\"fill\", function(d, i) { return fill(i); })\n" +
+                "            .attr(\"text-anchor\", \"middle\")\n" +
+                "            .attr(\"transform\", function(d) {\n" +
+                "                return \"translate(\" + [d.x, d.y] + \")rotate(\" + d.rotate + \")\";\n" +
+                "            })\n" +
+                "            .text(function(d) { return d.text; });\n" +
+                "    }\n" +
+                "</script>\n";
+        pw.print(html);
+
+        for (String dataName : normalizedData.keySet()) {
+            String jsonData = createJSONString(normalizedData.get(dataName));
+            String htmlData =
+                    "<html><h2>"  + dataName + "</h2></html>\n" +
+                    "<script>\n" +
+                    "    d3.layout.cloud().size([1024, 800])\n" +
+                    "        .words("   + jsonData +    ")\n" +
+                    "        .rotate(function() { return ~~(Math.random() * 2) * 90; })\n" +
+                    "        .font(\"Impact\")\n" +
+                    "        .fontSize(function(d) { return d.size; })\n" +
+                    "        .on(\"end\", draw)\n" +
+                    "        .start();\n" +
+                    "\n" +
+                    "</script>\n";
+            pw.print(htmlData);
+        }
+        pw.close();
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void setMinSize(int minSize) {
+        this.minSize = minSize;
+    }
 }
--- src/test/java/jevm/nutshell/tests/NutshellVisualizationTest.java
+++ src/test/java/jevm/nutshell/tests/NutshellVisualizationTest.java
@@ -9,7 +9,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,32 +22,48 @@ class NutshellVisualizationTest {
     void testCreateVisualization1() throws FileNotFoundException {
         File f1 = new File("res/stopwords_EN.txt");
         StopWordsFileReader r = new StopWordsFileReader(f1);
-        TextAnalyzer analyzer = new TextAnalyzer(r, "WEIGHTED_DEGREE");
+        TextAnalyzer analyzer = new TextAnalyzer(r, "DEGREE");
 
-        File f2 = new File ("res/lord_rings.txt");
+        File f2 = new File ("res/old_man.txt");
         FileWordParser fp = new FileWordParser(f2);
         analyzer.addText(fp);
 
-        List<ScoredWord> keywords = analyzer.getKeyWordsSingle(200);
+        List<ScoredWord> keywords = analyzer.getKeyWordsSingle(100);
 
-        NutshellVisualization v = new NutshellVisualization(keywords);
-        v.createJSONFile();
+        NutshellVisualization v = new NutshellVisualization();
+        v.addDataSet("Test file", keywords);
+        v.createWordCloud("nutshell.html");
     }
 
     @Test
     void testCreateVisualization2() throws FileNotFoundException {
-        File f1 = new File("res/stopwords_EN.txt");
-        StopWordsFileReader r = new StopWordsFileReader(f1);
-        TextAnalyzer analyzer = new TextAnalyzer(r, "WEIGHTED_DEGREE");
+
+        StopWordsFileReader r1 = new StopWordsFileReader(new File("res/stopwords_EN.txt"));
+        TextAnalyzer analyzer = new TextAnalyzer(r1, "WEIGHTED_DEGREE");
 
         File f2 = new File ("res/lord_rings.txt");
         FileWordParser fp = new FileWordParser(f2);
         analyzer.addText(fp);
 
-        List<ScoredWord> keywords = analyzer.getKeyWordsSingle(200);
+        List<ScoredWord> keywords = analyzer.getKeyWordsSingle(100);
 
-        NutshellVisualization v = new NutshellVisualization(keywords);
-        v.createJSONFile();
+        NutshellVisualization v = new NutshellVisualization();
+        v.addDataSet("Corpus", keywords);
+
+        StopWordsFileReader r2 = new StopWordsFileReader(new File("res/stopwords_EN.txt"));
+        TextAnalyzer analyzer2 = new TextAnalyzer(r2, "WEIGHTED_DEGREE");
+
+        File f3 = new File ("res/lord_rings.txt");
+        FileWordParser fp3 = new FileWordParser(f3);
+        File f4 = new File ("res/lord_rings_6.txt");
+        FileWordParser fp4 = new FileWordParser(f4);
+        analyzer2.addCorpus(fp3);
+        analyzer2.addText(fp4);
+
+        List<ScoredWord> keywords2 = analyzer2.getKeyWordsSingle(100);
+
+        v.addDataSet("Text", keywords2);
+        v.createWordCloud("nutshell.html");
     }
 
 }
